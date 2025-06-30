35afa8e1cac07b66d2a0fd222b02d8cd134b71e0
Senhaji Rhazi Hamza <hamza.senhajirhazi@gmail.com>
2025-06-30 21:12:40 +0200

    removed dynamic __version__ + add wheels for python 3.10,11

--- polodb/version.py
+++ polodb/version.py
@@ -1,12 +1,4 @@
-import tomli
 import os
 
 
-def get_version():
-    pyproject_path = os.path.join(os.path.dirname(__file__), "..", "pyproject.toml")
-    with open(pyproject_path, "rb") as f:
-        pyproject_data = tomli.load(f)
-    return pyproject_data["project"]["version"]  # Adjust if using a different tool
-
-
-__version__ = get_version()
+__version__ = "0.1.18"
